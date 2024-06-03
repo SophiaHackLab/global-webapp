d1199d1ceea0c90da884413c9df5bd6bd16e90e3
alexandre-vl <alexandre.vargas.lopez@gmail.com>
2024-06-03 14:45:19 +0200

    Fix typo in footer text

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,6 +2,6 @@
 
 <template>
     <footer class="bg-black text-white text-center p-4 pt-10">
-        <p>© 2024 SHL - Sophia HackLab</p>
+        <p>© 2024 SHL - Sophia Hack Lab</p>
     </footer>
 </template>
