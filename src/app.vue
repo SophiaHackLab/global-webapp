e5c6bbf5e0b8de50593437f4ba84236471cd35e0
alexandre-vl <alexandre.vargas.lopez@gmail.com>
2024-06-25 10:14:34 +0200

    refactor: Update meta tags in app.vue

--- src/app.vue
+++ src/app.vue
@@ -18,6 +18,23 @@ const head = {
             content:
                 "Le SHL (Sophia Hack Lab) est un hackerspace. Un lieu géré collaborativement par ses membres. Il fournit à Sophia Antipolis un environnement pour des projets techniques-créatifs.",
         },
+        {
+            name: "keywords",
+            content:
+                "SHL, Sophia Hack Lab, hackerspace, Sophia Antipolis, makerspace, hackerspace, hackerspace Sophia Antipolis, hackerspace France, hackerspace Alpes-Maritimes, hackerspace PACA, hackerspace Côte d'Azur, hackerspace French Riviera, hackerspace 06, hackerspace Vallauris, hackerspace Antibes, hackerspace Biot, hackerspace Valbonne, hackerspace Mougins, hackerspace Cannes, hackerspace Grasse, hackerspace Nice",
+        },
+        {
+            name: "author",
+            content: "SHL - Sophia Hack Lab",
+        },
+        {
+            name: "viewport",
+            content: "width=device-width, initial-scale=1.0",
+        },
+        {
+            name: "theme-color",
+            content: "#000000",
+        },
     ],
 };
 
