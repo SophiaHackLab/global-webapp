441d795632cb063b127af35131020a4fb3a206cc
alexandre-vl <alexandre.vargas.lopez@gmail.com>
2024-07-09 18:28:10 +0200

    refactor: Update TV mode QR page layout and styles

--- src/pages/tvmode/index.vue
+++ src/pages/tvmode/index.vue
@@ -7,7 +7,7 @@ definePageMeta({
 <template>
     <div class="flex flex-col gap-10 w-full items-center">
         <div class="w-full aspect-video">
-            <video class="w-full h-full" autoplay muted loop width="680" height="382">
+            <video class="w-full h-full" autoplay muted controls loop width="680" height="382">
                 <source
                     src="https://cloud.shl.contact/s/gfi89TbERHpSidZ/download/12-13-06-2024-BBQ.mp4"
                     type="video/mp4"
